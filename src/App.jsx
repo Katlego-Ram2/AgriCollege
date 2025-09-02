4936874ac5bc4999368c81a99c257e0d82668391
katlego-ram2 <rampedikatleho@gmail.com>
2025-09-02 11:53:46 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Products from "./components/Products";
-import About from "./components/About";
-import HowItWorks from "./components/HowItWorks";
-import Testimonial from "./components/Testimonial";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Features from "./components/Features.jsx";
+import Products from "./components/Products.jsx";
+import About from "./components/About.jsx";
+import HowItWorks from "./components/HowItWorks.jsx";
+import Testimonial from "./components/Testimonial.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
   return (
